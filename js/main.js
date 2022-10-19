5915ea56d202d656e91aaf5af45feb38a3f0d555
lucie chimits <chimitslucie@gmail.com>
2022-10-19 12:36:07 +0200

    finish home page with js

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// 1 - detecter le clic sur le filtre
+// 2 - ajouter la class "filter-modal__open" sur le body
+
+// Récupérer le bouton dans le HTML par son ID, on l'enregistre dans une variable filter
+let filter = document.getElementById('filter-btn');
+
+// On detecte le clique sur le bouton "Filtrer"
+filter.addEventListener('click', e => {
+
+    // On récupère l'élément body
+    let body = document.querySelector('body');
+
+    // On ajoute la class sur le body
+    body.classList.add('filter-modal__open');
+})
+
+let closeIcon = document.querySelector('.filter-modal .filter-modal__icon');
+
+closeIcon.addEventListener('click', e => {
+
+    // On récupère l'élément body
+    let body = document.querySelector('body');
+
+    // On ajoute la class sur le body
+    body.classList.remove('filter-modal__open');
+})
