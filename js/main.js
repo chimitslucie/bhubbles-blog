32a583dc41732317d78344df1e9972af6f9f8b2e
lucie chimits <chimitslucie@gmail.com>
2022-10-20 19:26:00 +0200

    start slider

--- js/main.js
+++ js/main.js
@@ -5,22 +5,30 @@
 let filter = document.getElementById('filter-btn');
 
 // On detecte le clique sur le bouton "Filtrer"
-filter.addEventListener('click', e => {
+if(filter) {
+    filter.addEventListener('click', e => {
 
-    // On récupère l'élément body
-    let body = document.querySelector('body');
+        // On récupère l'élément body
+        let body = document.querySelector('body');
+    
+        // On ajoute la class sur le body
+        body.classList.add('filter-modal__open');
+    })
+    
+    let closeIcon = document.querySelector('.filter-modal .filter-modal__icon');
+    
+    closeIcon.addEventListener('click', e => {
+    
+        // On récupère l'élément body
+        let body = document.querySelector('body');
+    
+        // On ajoute la class sur le body
+        body.classList.remove('filter-modal__open');
+    })
+}
 
-    // On ajoute la class sur le body
-    body.classList.add('filter-modal__open');
-})
 
-let closeIcon = document.querySelector('.filter-modal .filter-modal__icon');
-
-closeIcon.addEventListener('click', e => {
-
-    // On récupère l'élément body
-    let body = document.querySelector('body');
-
-    // On ajoute la class sur le body
-    body.classList.remove('filter-modal__open');
-})
+$('.gallery__slider').slick({
+    arrows: false,
+    dots: true
+});
